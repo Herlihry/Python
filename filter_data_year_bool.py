5b33f9291b2a7ffa3df62035bf57e6e261bd9d59
Herlihry <121634083+Herlihry@users.noreply.github.com>
2022-12-30 09:51:48 -0500

    Update filter_data_year_bool.py

--- filter_data_year_bool.py
+++ filter_data_year_bool.py
@@ -4,10 +4,10 @@ import pandas as pd
 df = pd.read_csv("data.csv")
 
 # filter for rows where the "number_column" is greater than 1999
-filtered_df = df[df["number_column"] > 1999]
+filtered_df = df[df["Year"] > 1999]
 
 # filter for rows where the "string_column" contains the word "Yes"
-filtered_df = filtered_df[filtered_df["string_column"].str.contains("Yes")]
+filtered_df = filtered_df[filtered_df["Bool"].str.contains("Yes")]
 
 # view the resulting dataframe
 print(filtered_df)
